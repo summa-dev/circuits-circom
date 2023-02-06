5b2efb9a2b273b76f800d415d7280c97c84b4448
enricobottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-02-06 21:04:59 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ sudo apt install nodejs npm
 
 # Install Rust and Circom
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+source "$HOME/.cargo/env"
 git clone https://github.com/iden3/circom.git
 cd circom
 cargo build --release
